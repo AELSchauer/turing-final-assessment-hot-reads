e944551319b208a0bf114c52a7a43d4398ad7a6f
Ashley Schauer <aelschauer@gmail.com>
2017-06-05 15:27:52 -0600

    add models for migrations

--- app/models/read.rb
+++ app/models/read.rb
@@ -1,5 +1,6 @@
 class Read < ApplicationRecord
   belongs_to :url
 
-  validates :user_id, :presence
+  validates :user_id, presence: true
+  validates_uniqueness_of :user_id, scope: :url_id, message: 'HotRead already recorded'
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,32 @@
 class Url < ApplicationRecord
   has_many :reads
 
-  validates :name, :presence
+  validates :address, presence: true
+  validates :address, uniqueness: true
+
+
+  def hot?
+    top_ten = Url.top_ten.keys
+    if top_ten[0] == id
+      'top'
+    elsif top_ten.include?(id)
+      'hot'
+    else
+      ''
+    end
+  end
+
+  def self.top_ten
+    yesterday = (Time.new.utc - (60 * 60 * 24)).strftime('%Y-%m-%d %H:%M:%S')
+    query = "SELECT urls.id, count(reads.id) FROM urls
+      INNER JOIN reads ON urls.id = reads.url_id
+      WHERE reads.created_at >= '#{yesterday}'
+      GROUP BY urls.id
+      ORDER BY count(reads.id) DESC
+      LIMIT 10
+      ;"
+
+    results = ActiveRecord::Base.connection.execute(query)
+    results.reduce({}) { |h, r| h.merge!({r['id'] => r['count']}) }
+  end
 end
