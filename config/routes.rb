0aeb17f384bbde9d243890bb6acae3e142c560c2
Ashley Schauer <aelschauer@gmail.com>
2017-06-05 15:28:24 -0600

    update database and configs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :read_urls, only: :create
+      resources :hot_reads, only: :index
+    end
+  end
 end
