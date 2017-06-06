156392ddbdec7c03ad798876bf4682672d89bcd3
Ashley Schauer <aelschauer@gmail.com>
2017-06-05 20:44:52 -0600

    fix bugs

--- app/controllers/api/v1/read_urls_controller.rb
+++ app/controllers/api/v1/read_urls_controller.rb
@@ -1,13 +1,15 @@
 class Api::V1::ReadUrlsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def create
     url = Url.find_or_create_by(url_params)
     if url.save
-      read = url.reads.new(read_params)
-      if read.save
-        render json: { url: url.address }, status: 201
-      else
-        render json: { error: read.errors.full_messages }, status: :bad_request
-      end
+        read = url.reads.new(read_params)
+        if read.save
+          render json: { url: url.address, user_id: read.user_id }, status: 201
+        else
+          render json: { url: url.address }, status: 201
+        end
     else
       render json: { error: url.errors.full_messages }, status: :bad_request
     end
@@ -20,6 +22,10 @@ class Api::V1::ReadUrlsController < ApplicationController
   end
 
   def read_params
-    params.require(:read).permit(:user_id)
+    begin
+      params.require(:read).permit(:user_id)
+    rescue
+      {}
+    end
   end
 end
